53948d96907564c555b53bed588812ef87f75919
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-10 21:42:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-    $('#contactForm').submit(function(e) {
-        e.preventDefault();
-        alert('Thank you for reaching out! We will get back to you soon.');
-    });
+// Import the express module
+const express = require('express');
+
+// Create an express app
+const app = express();
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Serve static files from the public folder
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  // Render the index.ejs file with some data
+  res.render('index', {title: 'Home', message: 'Welcome to Node.js and Express!'});
+});
+
+// Define a route for the about page
+app.get('/about', (req, res) => {
+  // Render the about.ejs file with some data
+  res.render('about', {title: 'About', message: 'This is a tutorial on full-stack development.'});
+});
+
+// Listen for requests on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
